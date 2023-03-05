a22584ba076ca58ba46cfe17317fbbcd499f3f93
ArcoKim <arcomade12@gmail.com>
2023-03-05 15:08:14 +0900

    230305 : db info update!

---
+++ part/mysql.php
@@ -0,0 +1,9 @@
+<?php
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+$conn = mysqli_connect(host, username, password, dbname, port);
+return $conn;
+?>
